530c83ee08487c915ec8ebb1c6bf823fc134a2af
Ghifari <ghifarikautsar8484@gmail.com>
2024-03-18 11:01:31 +0700

    feat(v1.3): Add division operation

--- PABW/script.js
+++ PABW/script.js
@@ -18,3 +18,14 @@ function kali() {
     let result = operand1 * operand2;
     document.getElementById("result").textContent = "Hasil: " + result;
 }
+
+function bagi() {
+    let operand1 = parseFloat(document.getElementById("operand1").value);
+    let operand2 = parseFloat(document.getElementById("operand2").value);
+    if (operand2 !== 0) {
+        let result = operand1 / operand2;
+        document.getElementById("result").textContent = "Hasil: " + result;
+    } else {
+        document.getElementById("result").textContent = "Error: Tidak bisa dibagi dengan nol";
+    }
+}
