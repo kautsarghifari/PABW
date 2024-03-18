6b5d18db7c7fb01e78b9d6631280960a56fbdaa9
Ghifari <ghifarikautsar8484@gmail.com>
2024-03-18 11:00:59 +0700

    feat(v1.2): Add multiplication operation

--- PABW/script.js
+++ PABW/script.js
@@ -11,3 +11,10 @@ function kurang() {
     let result = operand1 - operand2;
     document.getElementById("result").textContent = "Hasil: " + result;
 }
+
+function kali() {
+    let operand1 = parseFloat(document.getElementById("operand1").value);
+    let operand2 = parseFloat(document.getElementById("operand2").value);
+    let result = operand1 * operand2;
+    document.getElementById("result").textContent = "Hasil: " + result;
+}
