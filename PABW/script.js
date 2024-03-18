0c5ed98b217e36a63543c8b59b05574b9f9471eb
Ghifari <ghifarikautsar8484@gmail.com>
2024-03-18 10:59:44 +0700

    feat(v1.0): adding addition operation

---
+++ PABW/script.js
@@ -0,0 +1,18 @@
+function tambah() {
+    let operand1 = parseFloat(document.getElementById("operand1").value);
+    let operand2 = parseFloat(document.getElementById("operand2").value);
+    let result = operand1 + operand2;
+    document.getElementById("result").textContent = "Hasil: " + result;
+}
+function kurang() {
+    let operand1 = parseFloat(document.getElementById("operand1").value);
+    let operand2 = parseFloat(document.getElementById("operand2").value);
+    let result = operand1 - operand2;
+    document.getElementById("result").textContent = "Hasil: " + result;
+}
+function kali() {
+    let operand1 = parseFloat(document.getElementById("operand1").value);
+    let operand2 = parseFloat(document.getElementById("operand2").value);
+    let result = operand1 * operand2;
+    document.getElementById("result").textContent = "Hasil: " + result;
+}
